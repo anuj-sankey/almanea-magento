49a3e5a403bbcbadd0083684829005caa80e3d4e
anuj-sankey <88392130+anuj-sankey@users.noreply.github.com>
2021-08-09 10:05:35 +0530

    Schedule maintaince POST bug solved

--- app/code/Customs/RestApi/registration.php
+++ app/code/Customs/RestApi/registration.php
@@ -1,7 +1,6 @@
 <?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'RestApi', __DIR__);
-
-
+Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'RestApi',
+    __DIR__
+);
